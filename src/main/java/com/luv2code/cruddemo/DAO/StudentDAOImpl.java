12bd42a709d73e48b2c911d7e94370734e0f10f5
Raymond Vo <rvo@deloitte.com.au>
2024-09-03 20:10:49 +1000

    feat: refactor findAll query to sort by asc

--- src/main/java/com/luv2code/cruddemo/DAO/StudentDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/DAO/StudentDAOImpl.java
@@ -34,7 +34,7 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public List<Student> findAll() {
-        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student ORDER BY lastName asc", Student.class);
         return query.getResultList();
     }
 
