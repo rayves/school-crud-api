359ec0e39ff70356333831615f89bcbbbaab6abb
Raymond Vo <rvo@deloitte.com>
2024-11-29 18:16:32 +1100

    chore: fix formatting

--- src/main/java/com/rvo/schoolcrudapi/controller/StudentController.java
+++ src/main/java/com/rvo/schoolcrudapi/controller/StudentController.java
@@ -2,9 +2,15 @@ package com.rvo.schoolcrudapi.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +21,6 @@ import com.rvo.schoolcrudapi.model.Student;
 import com.rvo.schoolcrudapi.service.IStudentService;
 
 import lombok.AllArgsConstructor;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/")
